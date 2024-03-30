9980b2da46e96c1030386891aa5966e267e21e92
lautaromartinez04 <lautarom403@gmail.com>
2024-03-30 11:52:56 -0300

    contacto

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-const carouselContent = document.querySelector('.carousel-content');
-const slides = document.querySelectorAll('.slide');
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
-let counter = 0;
-const slideWidth = slides[0].clientWidth;
-const totalSlides = slides.length;
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
 
-nextBtn.addEventListener('click', () => {
-  counter = (counter + 1) % totalSlides;
-  moveCarousel();
+        const target = document.querySelector(this.getAttribute('href'));
+
+        target.scrollIntoView({
+            behavior: 'smooth', 
+            block: 'start'
+        });
+    });
 });
-
-prevBtn.addEventListener('click', () => {
-  counter = (counter - 1 + totalSlides) % totalSlides;
-  moveCarousel();
-});
-
-function moveCarousel() {
-  const offset = -counter * slideWidth;
-  carouselContent.style.transition = 'transform 0.5s ease-in-out';
-  carouselContent.style.transform = `translateX(${offset}px)`;
-}

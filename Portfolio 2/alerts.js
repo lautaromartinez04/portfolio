07ace90f7e1e0aeee968318d9165840734788c3d
lautaromartinez04 <lautarom403@gmail.com>
2024-04-08 12:14:50 -0300

    portfolio2

---
+++ Portfolio 2/alerts.js
@@ -0,0 +1,8 @@
+
+function futbol(){
+    Swal.fire({
+        title: "Me gusta jugar fútbol, deberia aprender, simpatizante de Boca Juniors y Manchester United",
+        html: "<i class='fas fa-heart'></i>",
+        imageAlt: "Custom image"
+    });
+}
